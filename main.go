3a196f5e2da7e0a8bf74856362a4d5c8f236b8c5
Naeem Shaikh <naeem.shaikh@thoughtworks.com>
2022-07-22 11:19:37 +0530

    add qty to main

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package ddd
 import (
 	"ddd/apple_pencil"
 	"ddd/cart"
+	"ddd/sony_wireless_headphone"
 )
 
 func main() {
 	c := cart.NewCart()
-	c.Add(apple_pencil.NewApplePencil())
+	c.Add(apple_pencil.NewApplePencil(), 1)
+	c.Add(sony_wireless_headphone.NewSonyWirelessHeadphone(), 1)
 }
